5fbf289d9f33fd143949bfedcb312f2526963d77
Avilova21 <avilova21dk@gmail.com>
2022-06-23 15:16:21 +0300

    - add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+function Header () {
+	return (
+		<header>
+			<div className="headerLeft">
+				<img width={40} height={40} src="/img/logo.png" alt="logo"/>
+				<div>
+					<h3>REACT SNEAKERS</h3>
+					<p>Магазин лучших кроссовок</p>
+				</div>
+			</div>
+
+			<ul className="headerRight">
+				<li>
+					<img width={18} height={18} src="/img/cart.svg" alt="cart"/>
+					<span> 1205 руб.</span>
+				</li>
+				<li>
+					<img width={18} height={18} src="/img/Union.svg" alt="Union"/>
+				</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
