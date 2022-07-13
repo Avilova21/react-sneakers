254786642588b965b92c59f432dcf270f546d2f3
Avilova21 <avilova21dk@gmail.com>
2022-07-13 22:51:14 +0300

    - add input filter for cart item

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
+import React from "react";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import Card from "./components/Card";
-
-const arr = [
-	{
-		title: "Мужские Кроссовки Nike Blazer Mid Suede",
-		price: 12999,
-		imageUrl: "/img/sneakers/1.jpg"
-	},
-	{
-		title: "Мужские Кроссовки Nike Air Max 270",
-		price: 12999,
-		imageUrl: "/img/sneakers/2.jpg"
-	},
-	{
-		title: "Мужские Кроссовки Nike Blazer Mid Suede",
-		price: 8499,
-		imageUrl: "/img/sneakers/3.jpg"
-	},
-	{
-		title: "Кроссовки Puma X Aka Boku Future Rider",
-		price: 8999,
-		imageUrl: "/img/sneakers/4.jpg"
-	}
-];
 
 function App() {
 	const [items, setItems] = React.useState([]);
@@ -56,20 +34,32 @@ function App() {
 
 			<div className="content">
 				<div className="search">
-					<h1>Все кроссовки</h1>
+					<h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
 					<div className="search-block">
 						<img src="/img/search.svg" alt="search"/>
-						<input placeholder="Поиск..."/>
+						{searchValue && (
+							<img
+								onClick={() => setSearchValue ('')}
+								className="removeBtn"
+								src="/img/btn-remove.svg"
+								alt="remove"
+							/>
+						)}
+						<input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."/>
 					</div>
 				</div>
 
 				<div className="all">
-					{arr.map((obj) => (
+					{items
+						.filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+						.map((item, index) => (
 						<Card
-							title={obj.title}
-							price={obj.price}
-							imageUrl={obj.imageUrl}
-							onClick={() => console.log(obj)}
+							key={index}
+							title={item.title}
+							price={item.price}
+							imageUrl={item.imageUrl}
+							onFavorite={() => console.log('Добавили закладки')}
+							onPlus={(obj) => onAddToCart(obj)}
 						/>
 					))}
 				</div>
