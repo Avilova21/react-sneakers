cb537351394567e6c71500ad4f74ae0e3b22c8df
Avilova21 <avilova21dk@gmail.com>
2022-07-17 15:14:07 +0300

    - add axios to component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import Card from "./components/Card";
@@ -10,17 +12,38 @@ function App() {
 	const [cartOpened, setCartOpened] = React.useState(false);
 
 	React.useEffect(() => {
-		fetch('https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/items')
-			.then((res) => {
-				return res.json();
-			})
-			.then((json) => {
-				setItems(json);
+		axios.get('https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/items').then(res => {
+			setItems(res.data);
 			});
+		axios.get('https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/cart').then(res => {
+			setCartItems(res.data);
+		});
+		axios.get('https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/favorites').then(res => {
+			setFavorites(res.data);
+		});
 	}, []);
 
 	const onAddToCart = (obj) => {
+		axios.post('https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/cart', obj);
 		setCartItems(prevState => [...prevState, obj]);
+	};
+
+	const onRemoveItem = (id) => {
+		axios.delete(`https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/cart/${id}`);
+		setCartItems((prev) => prev.filter(item => item.id !== id));
+	};
+
+	const onAddToFavorite = async (obj) => {
+		try {
+			if (favorites.find((favObj) => favObj.id === obj.id)) {
+				axios.delete(`https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/favorites/${obj.id}`);
+			} else {
+				const { data } = await axios.post('https://62bc12c6eff39ad5ee1bc7e3.mockapi.io/favorites', obj);
+				setFavorites(prevState => [...prevState, data]);
+			}
+		} catch (error) {
+			alert('Не удалось добавить в фавориты');
+		}
 	};
 
 	const onChangeSearchInput = (event) =>{
