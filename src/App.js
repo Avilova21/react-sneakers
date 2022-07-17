69bfe26a140c776a77271a4e9a46affcc883e6a7
Avilova21 <avilova21dk@gmail.com>
2022-07-17 15:20:27 +0300

    - add router to component

--- src/App.js
+++ src/App.js
@@ -55,40 +55,25 @@ function App() {
 			{cartOpened && <Drawer items={cartItems} onClose = {() => setCartOpened(false)}/> }
 			<Header onClickCart = {() => setCartOpened(true)} />
 
-			<div className="content">
-				<div className="search">
-					<h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
-					<div className="search-block">
-						<img src="/img/search.svg" alt="search"/>
-						{searchValue && (
-							<img
-								onClick={() => setSearchValue ('')}
-								className="removeBtn"
-								src="/img/btn-remove.svg"
-								alt="remove"
-							/>
-						)}
-						<input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."/>
-					</div>
-				</div>
+			<Route path="/" exact>
+				<Home
+					items={items}
+					searchValue={searchValue}
+					setSearchValue={setSearchValue}
+					onChangeSearchInput={onChangeSearchInput}
+					onAddToFavorite={onAddToFavorite}
+					onAddToCart={onAddToCart}
+				/>
+			</Route>
 
-				<div className="all">
-					{items
-						.filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
-						.map((item, index) => (
-						<Card
-							key={index}
-							title={item.title}
-							price={item.price}
-							imageUrl={item.imageUrl}
-							onFavorite={() => console.log('Добавили закладки')}
-							onPlus={(obj) => onAddToCart(obj)}
-						/>
-					))}
-				</div>
-			</div>
+			<Route path="/favorites" exact>
+				<Favorites
+					items={favorites}
+					onAddToFavorite={onAddToFavorite}
+				/>
+			</Route>
 		</div>
-	)
+	);
 }
 
 export default App;
